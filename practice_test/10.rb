9bd53811aedc7abca9b7a7d44e2822507a19ae0d
richwynmorris <rwmorris.bjj@gmail.com>
2020-04-13 10:28:10 +0100

    parsing code in preparation for written assessment

--- practice_test/10.rb
+++ practice_test/10.rb
@@ -6,7 +6,7 @@
 # method is invoked. Why? Fix the code so that it behaves as expected.
 
 def predict_weather
-  sunshine = ['true', 'false'].sample
+  sunshine = [true, false].sample
 
   if sunshine
     puts "Today's weather will be sunny!"
@@ -14,3 +14,25 @@ def predict_weather
     puts "Today's weather will be cloudy!"
   end
 end
+
+
+# On lines 1-9, the method predict_weather is defined with no parameters. 
+
+# On line 2, the local variable sunshine is initialized and is assigned the return
+# value of the array containing, the string objects 'true' or 'false'. The array
+# object calls the sample method which selects one of the elements passed to it as
+# an argument at random and returns it. 
+
+# On line 4, an if statement evaluates if the variable sunshine is true or not. 
+
+# As everything in Ruby is considered to be a truthy, except for false and nil,
+# the if statement always evaluates to true as the string objects, despite containing
+# the characters that make the words, true and false, both evaluate to true.
+# As a result, sunshine, whether it is assigned the value 'true' or 'false' 
+# will always evaluate to true, therefore 'Today's weather will be sunny!' will always be
+# passed to the puts method as an arguement and be output. This will also return the value nil.
+
+# To fix this code you must change the values in the array to boolean values. 
+
+# Example: [true, false]
+
--- practice_test/9.rb
+++ practice_test/9.rb
@@ -14,3 +14,20 @@ end
 
 find_first_nonzero_among(0, 0, 1, 0, 2, 0)
 find_first_nonzero_among(1)
+
+
+# On lines 1 - 5, the method find_first_nonzero_among is defined with one parameter, numbers.
+
+# On line 9, find_first_nonzer_among is invoked and the integers, 0, 0, 1, 0, 2, 0 are passed as
+# arguments to the method. As these are seperate elements, the method is passed 6 different objects as
+# arguments to the method. However, the method only accepts 1 parameter. This returns an error message
+# 'wrong number of arguments.' 
+
+
+# On line 10, find_first_nonzero_among is invoked and the integer 1 is passed to it as an argument.
+# On line 2 within the method, the parameter, numbers, calls the each method using the . operator. 
+# The parameter numbers is referencing the interget 1. The .each method is not available to the integer 
+# class and, as a result, the error 'undefined method' is returned. 
+
+# To fix this issue, the integers must be passed in using either an array or hash object for the method
+# to accept one argument and for the .each method to be invoked. 
